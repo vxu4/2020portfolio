af89ea3e65e71c7656974aaa4510ac0311744e89
vxu4 <vixixu68@gmail.com>
2020-09-26 08:18:06 -0400

    Updates

---
+++ precache-manifest.1a9b113148cec0e22941b59e61e60364.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a298c4cea27fc7fed4aade412e785207",
+    "url": "/2020portfolio/index.html"
+  },
+  {
+    "revision": "ed1839d3aa08408eefbd",
+    "url": "/2020portfolio/static/css/2.3ab4c8ae.chunk.css"
+  },
+  {
+    "revision": "ce84fe2791c7cbaa5a64",
+    "url": "/2020portfolio/static/css/main.4a49afab.chunk.css"
+  },
+  {
+    "revision": "ed1839d3aa08408eefbd",
+    "url": "/2020portfolio/static/js/2.75197b40.chunk.js"
+  },
+  {
+    "revision": "6a2bc4dfc852217704ff5945e37d112d",
+    "url": "/2020portfolio/static/js/2.75197b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce84fe2791c7cbaa5a64",
+    "url": "/2020portfolio/static/js/main.572242e8.chunk.js"
+  },
+  {
+    "revision": "9554f94d2cfaa13ab465",
+    "url": "/2020portfolio/static/js/runtime-main.2bf4c0b2.js"
+  },
+  {
+    "revision": "d9f4498f547154ed464d41075170a0e8",
+    "url": "/2020portfolio/static/media/VXXBanner_md.d9f4498f.png"
+  },
+  {
+    "revision": "afa4ab27acc2d62dbdf5821a0881b778",
+    "url": "/2020portfolio/static/media/VXX_sm.afa4ab27.png"
+  },
+  {
+    "revision": "764e7c0dd0ed16c8c1cd37ec7bcdd768",
+    "url": "/2020portfolio/static/media/arrow.764e7c0d.svg"
+  },
+  {
+    "revision": "51b7e1b38e4f3eec2fabb36f4ad01fb4",
+    "url": "/2020portfolio/static/media/flowerNet.51b7e1b3.jpg"
+  },
+  {
+    "revision": "38e48a1df8fe793ef83f6eada60b3b06",
+    "url": "/2020portfolio/static/media/home_artPic.38e48a1d.png"
+  },
+  {
+    "revision": "5e734d4a7393837a57002a8009d6ebe8",
+    "url": "/2020portfolio/static/media/home_artPic.5e734d4a.jpg"
+  },
+  {
+    "revision": "d6659b1a9dd70ab772989bbc0c0d5e62",
+    "url": "/2020portfolio/static/media/home_devPic.d6659b1a.png"
+  },
+  {
+    "revision": "b291eebc0ff1d2d332634d419ed543ff",
+    "url": "/2020portfolio/static/media/home_filmPic.b291eebc.png"
+  },
+  {
+    "revision": "9952f6832865ebb1e135551a56d29ade",
+    "url": "/2020portfolio/static/media/home_landingPic_2.9952f683.jpg"
+  },
+  {
+    "revision": "170378d5d0453ce87de6cdfcbfd70e6e",
+    "url": "/2020portfolio/static/media/home_landingPic_3.170378d5.jpg"
+  },
+  {
+    "revision": "5e18b098c94ed5937554f2aee59bea68",
+    "url": "/2020portfolio/static/media/lattice.5e18b098.jpg"
+  },
+  {
+    "revision": "5a44cdd55ce67aba5fb8d49227dbe805",
+    "url": "/2020portfolio/static/media/redStory.5a44cdd5.png"
+  }
+]);
