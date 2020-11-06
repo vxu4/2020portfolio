db9fcb59eec41addaa254ceaa0d811a203e70c77
vxu4 <vixixu68@gmail.com>
2020-11-06 02:22:58 -0500

    fixing spacing for design

--- src/pages/Design_Overall.jsx
+++ src/pages/Design_Overall.jsx
@@ -4,8 +4,8 @@ import discover_1 from "../assets/design/discover_1.png";
 import malloc_3 from "../assets/design/malloc_3.png";
 
 export const Design_Overall = () => (
-  <div className="py-5 my-5 w-100 h-100 d-flex flex-column ">
-    <div className="w-100 px-5 d-flex flex-row align-items-center justify-space-between">
+  <div className="py-5 my-5 w-100 h-100 d-flex flex-column align-items-center">
+    <div className="w-75 pt-5 px-5 d-flex flex-row align-items-center justify-content-between">
       <img src={clickstream} alt="clickstream icon" style={{height: '60vh'}} />
       <img src={discover_1} alt="clickstream icon" style={{height: '60vh'}} />
       <img src={malloc_3} alt="clickstream icon" style={{height: '60vh'}}  />
